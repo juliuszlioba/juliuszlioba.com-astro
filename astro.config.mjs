d97f452ab539bd4575304de8650190d39f67d5b1
Julius Zlioba <julius.zlioba@connect724.at>
2023-01-25 12:46:05 +0100

    v1.1.0: updated dependencies, astro v2

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,17 +4,12 @@ import { defineConfig } from 'astro/config';
 import preact from "@astrojs/preact";
 import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
-import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind(),
-    preact({
-      compat: true,
-    }),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-  ],
+  integrations: [tailwind(), preact({
+    compat: true
+  }), image({
+    serviceEntryPoint: "@astrojs/image/sharp"
+  })]
 });
