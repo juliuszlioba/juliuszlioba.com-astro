b88ddf72fafed518718d91e3230ce4f6012e4f2d
juliuszlioba <julius.zlioba@gmail.com>
2023-09-03 23:30:35 +0200

    translation in progress

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -12,20 +12,36 @@ export const ui = {
     "nav.works": "Works",
     "nav.services": "Services",
     "nav.contacts": "Contacts",
+    "nav.privacy_policy": "Privacy Policy",
+    "nav.consent_settings": "Consent Settings",
+    "nav.back_btn": "Go back",
+    "footer.header": "Contact me",
+    "footer.text": "Have a project? Get in touch!",
+    "error.404": "Page not found",
   },
   lt: {
     "nav.abaut": "Apie",
     "nav.works": "Darbai",
     "nav.services": "Paslaugos",
     "nav.contacts": "Kontaktai",
+    "nav.privacy_policy": "Privatumo politika",
+    "nav.consent_settings": "Sutikimo nustatymai",
+    "nav.back_btn": "Grįžk atgal",
+    "footer.header": "Susisiek su manimi",
+    "footer.text": "Turi projektą? Kreipkis!",
+    "error.404": "Puslapis nerastas",
   },
 } as const;
 
 export const routes = {
-  de: {
+  en: {
     contacts: "contacts",
+    "privacy-policy": "privacy-policy",
+    copyright: "copyright",
   },
   lt: {
     contacts: "kontaktai",
+    "privacy-policy": "privatumo-politika",
+    copyright: "autorines-teises",
   },
 };
