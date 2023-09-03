426b149e40ce3100928112c9fc603210f7d16504
juliuszlioba <julius.zlioba@gmail.com>
2023-09-03 21:24:13 +0200

    added translation logic

---
+++ src/i18n/ui.ts
@@ -0,0 +1,31 @@
+export const languages = {
+  en: "En",
+  lt: "Lt",
+};
+
+export const showDefaultLang = false;
+export const defaultLang = "en";
+
+export const ui = {
+  en: {
+    "nav.abaut": "About",
+    "nav.works": "Works",
+    "nav.services": "Services",
+    "nav.contacts": "Contacts",
+  },
+  lt: {
+    "nav.abaut": "Apie",
+    "nav.works": "Darbai",
+    "nav.services": "Paslaugos",
+    "nav.contacts": "Kontaktai",
+  },
+} as const;
+
+export const routes = {
+  de: {
+    contacts: "contacts",
+  },
+  lt: {
+    contacts: "kontaktai",
+  },
+};
